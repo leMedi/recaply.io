efa043cc266ffe693109967d2894e2442063ff40
LeMedi <mehdi.elhaij@outlook.com>
2024-05-22 03:32:09 +0200

    fix: footer border

--- apps/nextjs/src/app/(marketing)/_components/footer.tsx
+++ apps/nextjs/src/app/(marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Footer = () => {
 	return (
 		<footer id="footer">
-			<hr className="w-11/12 mx-auto" />
+			<hr />
 
 			<section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
 				<div className="col-span-full xl:col-span-2">
